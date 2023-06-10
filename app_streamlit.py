ad45aecef5ce62d9014e5967922462cf4165c592
han.azha99 <han.azha99@gmail.com>
2023-06-10 09:32:29 +0000

    update

---
+++ app_streamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+Home, Implementasi = st.tabs(['Home','Implementasi'])
+
+with Home:
+   st.title("""Time Series Temperatur Ruangan""")
+   st.text("""
+            1. Zuni Amanda Dewi 200411100051
+            2. Abd. Hanif Azhari 200411100101""")
